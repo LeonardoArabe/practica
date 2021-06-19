5197c98f42e7f7038800a9ee9402fbd91e2ea30b
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-06-18 22:16:34 -0400

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+void main()=>runApp(MiApp());
+class MiApp extends StatelessWidget {
+  const MiApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mi App", 
+      home: Inicio(),
+    );
+  }
+}
+class Inicio extends StatefulWidget {
+  Inicio({Key key}) : super(key: key);
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title:Text("Leonardo App")
+       ),
+       body: Center(
+         child
+       ),
+    );
+  }
+}
